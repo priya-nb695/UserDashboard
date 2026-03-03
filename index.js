6a9fd470e0c08818893477651ad4134401b89ce3
priyanb <priyanb250@gmail.com>
2026-03-03 22:15:10 +0530

    started js logic

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
-let loadButton=document.getElementsByClassName("spinner");
-let spinner=document.getElementsByClassName("spinner");
-let userInfoSection=getElementsByClassName("user-info-section");
-let userInfoSection=getElementsByClassName("posts-section");
+let loadButton=document.querySelector(".load-button");
+let spinnerSection=document.querySelector(".loader-section");
+let userInfoSection=document.querySelector(".user-info-section");
+let postSection=document.querySelector(".posts-section");
+
+loadButton.addEventListener("click",()=>{
+    spinnerSection.style.display="block";
+    
+    
+})
+
+const getUser = ()=>{
+    const userObj={};
+    setTimeout(()=>{
+        userObj={
+            id:1,
+            name:"Anu",
+            email:"anu@example.com"
+        }
+    },1000);
+  const userPosts=getPosts(userObj.id);
+
+     
+}
+
+const getPosts = (userId)=>{
+      if(userId){
+            return ({
+            data:[
+            {
+                id:1,
+                name:"My New Post",
+            },
+            {
+                id:2,
+                name:"My Second Post",
+            },
+        ]})
+      }
+      else{
+        return ({message:"User not found"})
+      }
+     
+
+}
+const getComments = (postId)=>{
+    if(postId){
+            return ([
+        {
+            id:1,
+            name:"Amazing Post",
+        },
+        {
+            id:2,
+            name:"Stunning Pic",
+        }
+     ])
+    }
+
+    else{
+         return ({message:"Post not found"})
+    }
+ 
+    
+ 
+}
