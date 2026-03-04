1b38bfee95d4bc8ff249448c5cfa8cae6862d2a4
priyanb <priyanb250@gmail.com>
2026-03-04 21:29:38 +0530

    small fixes

--- index.js
+++ index.js
@@ -1,83 +1,82 @@
-let loadButton=document.querySelector(".load-button");
-let loader=document.querySelector(".loader");
-let userInfoSection=document.querySelector(".user-info-section");
-let postSection=document.querySelector(".posts-section");
+let loadButton = document.querySelector(".load-button");
+let loader = document.querySelector(".loader");
+let userInfoSection = document.querySelector(".user-info-section");
+let postSection = document.querySelector(".posts-section");
 
-loadButton.addEventListener("click",()=>{
-    loader.style.display="flex";
-    document.querySelector(".loading-text").innerHTML="Loading..."
-    
-    
+loadButton.addEventListener("click", () => {
+    loader.style.display = "flex";
+    document.querySelector(".loading-text").innerHTML = "Loading..."
+
+
 })
 
-const getUser = (getPosts)=>{
-    const  userObj={
-            id:1,
-            name:"Anu",
-            email:"anu@example.com"
-        }
-
-      const fullPostObject= getPosts(userObj.id,getComments);
-      const fullUserObject = {
-        user:userObj,
-        posts:fullPostObject.posts,
-        comments:fullPostObject.comments,
-      }
-
-      return fullUserObject;
-    } 
-
- const getPosts = (userId,getComments)=>{
-            if(userId){
-                   
-                 const  posts=[
-                    {
-                        id:1,
-                        name:"My New Post",
-                    },
-                    {
-                        id:2,
-                        name:"My Second Post",
-                    },
-                    ]
-
-                 const comments = getComments(posts[0].id);
-                 fullPostObject={
-                    posts:posts,
-                    comments:comments
-                 }
-
-                 return fullPostObject;
-            }
-            else{
-                return ({message:"Post not found"})
-            }
-
-         
-
- }
-
-const getComments = (postId)=>{
-    if(postId){
-            return ([
-        {
-            id:1,
-            name:"Amazing Post",
-        },
-        {
-            id:2,
-            name:"Stunning Pic",
-        }
-     ])
+const getUser = (getPosts) => {
+    const userObj = {
+        id: 1,
+        name: "Anu",
+        email: "anu@example.com"
     }
 
-    else{
-         return ({message:"Post not found"})
+    const fullPostObject = getPosts(userObj.id, getComments);
+    const fullUserObject = {
+        user: userObj,
+        posts: fullPostObject.posts,
+        comments: fullPostObject.comments,
     }
- 
+
+    return fullUserObject;
 }
-    
-const obj=getUser(getPosts)
+
+const getPosts = (userId, getComments) => {
+    if (userId) {
+
+        const posts = [
+            {
+                id: 1,
+                name: "My New Post",
+            },
+            {
+                id: 2,
+                name: "My Second Post",
+            },
+        ]
+
+        const comments = getComments(posts[0].id);
+        fullPostObject = {
+            posts: posts,
+            comments: comments
+        }
+
+        return fullPostObject;
+    }
+    else {
+        return ({ message: "Post not found" })
+    }
+
+}
+
+const getComments = (postId) => {
+    if (postId) {
+        return ([
+            {
+                id: 1,
+                name: "Amazing Post",
+            },
+            {
+                id: 2,
+                name: "Stunning Pic",
+            }
+        ])
+    }
+
+    else {
+        return ({ message: "Post not found" })
+    }
+
+}
+
+const obj = getUser(getPosts);
+console.log(obj);
 
 
 
