3eb6afbf9e60170861cb8c24b975bb55dbbb2969
priyanb <priyanb250@gmail.com>
2026-03-06 22:33:18 +0530

    minor fix

--- index.js
+++ index.js
@@ -96,38 +96,7 @@ const getComments = (postId) => {
     }
 
 }
-
-function getUser1(getPosts1){
-    setTimeout(()=>{
-        const user={
-            id:1,
-            name:"anuu",
-        }
-      //callback(user);
-      getPosts1(user.id,(post) => {console.log("the posts are ",post)});
-    },1000)
-}
-
-// getUser1((user)=>{console.log("the user is",user)});
-
-function getPosts1(userID,callback){
-     setTimeout(()=>{
-        if(userID){
-        const post=[
-           {
-            id:1,
-            name:"first post",
-           },
-           {
-            id:1,
-            name:"second post",
-           }
-        ]
-           callback(post);
-        }
-       
-     
-    },1000);
-}
-getUser1(getPosts1);
+//  getUser1((user)=>{
+//     getPosts1(pos)
+//  })
 
