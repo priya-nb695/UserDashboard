931428557a96b5ec335ea41306ec284d4310f9c7
priyanb <priyanb250@gmail.com>
2026-03-07 20:29:28 +0530

    callback done

--- index.js
+++ index.js
@@ -2,74 +2,87 @@ let loadButton = document.querySelector(".load-button");
 let loader = document.querySelector(".loader");
 let userInfoSection = document.querySelector(".user-info-section");
 let postSection = document.querySelector(".posts-section");
+let loaderSection = document.querySelector(".loader-section");
 
 loadButton.addEventListener("click", () => {
     loader.style.display = "flex";
     document.querySelector(".loading-text").innerHTML = "Loading...";
-    const obj = getUser(getPosts);
-    console.log(obj);
+    // const obj = getUser(getPosts);
+    getUser((user)=>{
+    console.log("the user",user);
+    getPosts(user.id,(posts)=>{
+        console.log("the posts",posts);
+        getComments(posts[0].id,(comments)=>{
+            console.log("the comments",comments);
 
-    if(Object.entries(obj.user).length>0){
-        userInfoSection.style.display='block';
-         document.querySelector(".name").innerHTML=obj.user.name;
-         document.querySelector(".email").innerHTML=obj.user.email;
-    }
-     if(obj.posts.length>0){
-        postSection.style.display='block';
-         document.querySelector(".post-title").innerHTML=obj.posts[0].name;
-       
-    }
-    if(obj.comments.length>0){
-       const comments=document.querySelectorAll(".comments li");
-       comments.forEach((item ,index)=> {
-            item.innerHTML=obj.comments[index].name;
-       })
-   
-    }
-    if(Object.entries(obj).length>0){
-        let emptySection = document.querySelector(".empty-section");
-        emptySection.style.display='none';
-    }
+            
+                const obj = {
+                    user: user,
+                    posts: posts,
+                    comments:comments,
+                }
+
+                if(Object.entries(obj.user).length>0){
+                    userInfoSection.style.display='block';
+                    document.querySelector(".name").innerHTML=obj.user.name;
+                    document.querySelector(".email").innerHTML=obj.user.email;
+                }
+
+                if(obj.posts.length>0){
+                    postSection.style.display='block';
+                    document.querySelector(".post-title").innerHTML=obj.posts[0].name;
+                
+                }
+
+                if(obj.comments.length>0){
+                const comments=document.querySelectorAll(".comments li");
+                comments.forEach((item ,index)=> {
+                        item.innerHTML=obj.comments[index].name;
+                })
+                }
+
+                if(Object.entries(obj).length>0){
+                    let emptySection = document.querySelector(".empty-section");
+                    emptySection.style.display='none';
+                    loaderSection.style.display = "none";
+                }
+      
+            })
+    })
 })
-//need to add error cases n fail cases
-const getUser = (getPosts) => {
-    const userObj = {
+
+})
+
+const getUser = (callback) => {
+
+    setTimeout(()=>{
+        const user = {
         id: 1,
         name: "Anu",
         email: "anu@example.com"
-    }
-
-    const fullPostObject = getPosts(userObj.id, getComments);
-    const fullUserObject = {
-        user: userObj,
-        posts: fullPostObject.posts,
-        comments: fullPostObject.comments,
-    }
-
-    return fullUserObject;
+     }
+     callback(user);
+    },1000)
+  
 }
 
-const getPosts = (userId, getComments) => {
+const getPosts = (userId,callback) => {
     if (userId) {
 
-        const posts = [
+        setTimeout(()=>{
+         const posts = [
             {
                 id: 1,
                 name: "My New Post",
             },
-            // {
-            //     id: 2,
-            //     name: "My Second Post",
-            // },
-        ]
-
-        const comments = getComments(posts[0].id);
-        fullPostObject = {
-            posts: posts,
-            comments: comments
-        }
-
-        return fullPostObject;
+            {
+                id: 2,
+                name: "My Second Post",
+            },
+          ]
+          callback(posts);
+        },1000)
+   
     }
     else {
         return null;
@@ -77,9 +90,11 @@ const getPosts = (userId, getComments) => {
 
 }
 
-const getComments = (postId) => {
+
+const getComments = (postId,callback) => {
     if (postId) {
-        return ([
+        setTimeout(()=>{
+            const comments= [
             {
                 id: 1,
                 name: "Amazing Post",
@@ -88,7 +103,10 @@ const getComments = (postId) => {
                 id: 2,
                 name: "Stunning Pic",
             }
-        ])
+        ]
+        callback(comments);
+        },1000)
+        
     }
 
     else {
@@ -96,7 +114,6 @@ const getComments = (postId) => {
     }
 
 }
-//  getUser1((user)=>{
-//     getPosts1(pos)
-//  })
+
+//callback is passed as params to another function
 
