48c41799b8e39c25a235f5d620a26d22eb36b3a6
priyanb <priyanb250@gmail.com>
2026-03-07 22:18:56 +0530

    started promise mehtod

--- index.js
+++ index.js
@@ -8,121 +8,119 @@ loadButton.addEventListener("click", () => {
     loader.style.display = "flex";
     document.querySelector(".loading-text").innerHTML = "Loading...";
 
-    getUser((user)=>{
+    getUser((user) => {
 
-    getPosts(user.id,(posts)=>{
+        getPosts(user.id, (posts) => {
 
-        getComments(posts[0].id,(comments)=>{
-         
+            getComments(posts[0].id, (comments) => {
+
                 const obj = {
                     user: user,
                     posts: posts,
-                    comments:comments,
+                    comments: comments,
                 }
 
-                if(Object.entries(obj.user).length>0){
-                    userInfoSection.style.display='block';
-                    document.querySelector(".name").innerHTML=obj.user.name;
-                    document.querySelector(".email").innerHTML=obj.user.email;
+                if (Object.entries(obj.user).length > 0) {
+                    userInfoSection.style.display = 'block';
+                    document.querySelector(".name").innerHTML = obj.user.name;
+                    document.querySelector(".email").innerHTML = obj.user.email;
                 }
 
-                if(obj.posts.length>0){
-                    postSection.style.display='block';
-                    document.querySelector(".post-title").innerHTML=obj.posts[0].name;
-                
+                if (obj.posts.length > 0) {
+                    postSection.style.display = 'block';
+                    document.querySelector(".post-title").innerHTML = obj.posts[0].name;
+
                 }
 
-                if(obj.comments.length>0){
-                const commentsItem=document.querySelectorAll(".comments li");
-                commentsItem.forEach((item ,index)=> {
-                        item.innerHTML=obj.comments[index].name;
-                })
+                if (obj.comments.length > 0) {
+                    const commentsItem = document.querySelectorAll(".comments li");
+                    commentsItem.forEach((item, index) => {
+                        item.innerHTML = obj.comments[index].name;
+                    })
                 }
 
-                if(Object.entries(obj).length>0){
+                if (Object.entries(obj).length > 0) {
                     let emptySection = document.querySelector(".empty-section");
-                    emptySection.style.display='none';
+                    emptySection.style.display = 'none';
                     loader.style.display = "none";
                 }
-      
+
             })
+        })
     })
-})
 
 })
 
 const getUser = () => {
 
-   return new Promise((resolve,reject)=>{
-            setTimeout(()=>{
-                const user = {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const user = {
                 id: 1,
                 name: "Anu",
                 email: "anu@example.com"
-        }
-          resolve(user);
-        },1000)
-   }) 
-  
+            }
+            resolve(user);
+        }, 1000)
+    })
+
 }
 
-const getPosts = (userId,callback) => {
-   
-   return new Promise((resolve,reject)=>{
-    
-    
-    if(userId){
-     setTimeout(()=>{
-        if(userId){     const posts = [
-            {
-                id: 1,
-                name: "My New Post",
-            },
-            {
-                id: 2,
-                name: "My Second Post",
-            },
-          ]
-          resolve(posts);
+const getPosts = (userId, callback) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (userId) {
+            setTimeout(() => {
+                if (userId) {
+                    const posts = [
+                        {
+                            id: 1,
+                            name: "My New Post",
+                        },
+                        {
+                            id: 2,
+                            name: "My Second Post",
+                        },
+                    ]
+                    resolve(posts);
+                }
+            }, 1000);
         }
-        },1000);
+        else {
+            reject("User id not found")
+        }
+
     }
-    else{
-        reject("User id not found")
-    }
+
+    )
+}
+
+const getComments = (postId, callback) => {
+    return new Promise((resolve, reject) => {
+
+        if (postId) {
+            setTimeout(() => {
+                const comments = [
+                    {
+                        id: 1,
+                        name: "Amazing Post",
+                    },
+                    {
+                        id: 2,
+                        name: "Stunning Pic",
+                    }
+                ]
+                resolve(comments);
+            }, 1000)
+
+        }
+
+        else {
+            reject("Post Id not found");
+        }
         
-            
-   
-     }
-     
-  
-   )}
-
-    
-const getComments = (postId,callback) => {
-  return new Promise((resolve,reject)=>{
-
-    if (postId) {
-        setTimeout(()=>{
-            const comments= [
-            {
-                id: 1,
-                name: "Amazing Post",
-            },
-            {
-                id: 2,
-                name: "Stunning Pic",
-            }
-        ]
-       resolve(comments);
-        },1000)
-        
-    }
-
-     else {
-       reject("Post Id not found");
-     }
-  })  
+    })
 
 }
 
